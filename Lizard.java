e0a8390d3c1f768e37653fb23929e4dd16581605
dceddy7 <33559490+dceddy7@users.noreply.github.com>
2018-10-11 10:41:52 -0500

    Added Comments

--- Lizard.java
+++ Lizard.java
@@ -3,6 +3,7 @@ package de.fu_berlin.inf.dpp.ui.widgets.chat;
 import org.apache.log4j.Logger;
 
 public class Lizard {
+    //The IP address
     String ip_addr;
     FacsvatarClientLauncher client;
     private static final Logger LOG = Logger.getLogger(Lizard.class);
@@ -12,7 +13,8 @@ public class Lizard {
         LOG.debug("[SUCC] Lizard FACSvatar finalizing");
         client.KillSubProcesses();
     }
-
+    
+    //Starts FACSvatar
     public Lizard(String ip_addr) {
         this.ip_addr = ip_addr;
         LOG.debug("[SUCC] Lizard FACSvatar starting up");
