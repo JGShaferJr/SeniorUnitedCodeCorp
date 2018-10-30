2b7336e92383c73a00885e9a368a6ad2f04b8faf
jgshaferjr <jgs6303@utulsa.edu>
2018-10-30 10:26:53 -0500

    uploading .bat files from sprint2

---
+++ BatchFiles/run_n_proxy.bat
@@ -0,0 +1,2 @@
+
+python C:\Users\jgsha\git\saros\de.fu_berlin.inf.dpp\FACsvatar_client_interface\modules\n_proxy_m_bus.py --sub_ip %2
---
+++ BatchFiles/run_pub_blend.bat
@@ -0,0 +1,2 @@
+cd %1
+python C:\Users\jgsha\git\saros\de.fu_berlin.inf.dpp\FACsvatar_client_interface\modules\process_facstoblend\pub_blend.py
